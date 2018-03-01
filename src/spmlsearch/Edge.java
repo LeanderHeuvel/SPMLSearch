75f4eb4e717e7cf5c3349b19338aa270c75612eb
Unknown <leander.vandenheuvel@gmail.com>
2018-03-01 12:21:12 +0100

    Auto stash before merge of "master" and "origin/LeanderHeuvel"

---
+++ src/spmlsearch/Edge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spmlsearch;
+
+/**
+ *
+ * @author Jaap van Bussel
+ * @author Leander van den Heuvel
+ */
+public class Edge {
+    private int x1,x2;
+    private int y1,y2;
+    public Edge(Vertex vertex1, Vertex vertex) {
+        
+    }
+}
